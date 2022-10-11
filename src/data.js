f3ef49256ddcfaa9a081726f8ae3e83e2098816b
appszonemusic <103084583+appzonemusic@users.noreply.github.com>
2022-10-10 17:13:49 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ function chillHop() {
         {
             name:"Quien es un Líder Regional",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-que-es-un-lider-regional-podcast-1.mp3",
@@ -15,7 +15,7 @@ function chillHop() {
         {
             name:"Que es ser Voluntario a tiempo completo en el Movimiento Tengo Sed",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-que-es-ser-voluntario-a-tiempo-completo-en-el-movimiento-tengo-sed-1.mp3",
@@ -24,7 +24,7 @@ function chillHop() {
         {
             name:"Comunidades Juveniles en el MTS PRIMERA PARTE",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-comunidades-juveniles-en-el-mts-primera-parte-1.mp3",
@@ -33,7 +33,7 @@ function chillHop() {
         {
             name:"Comunidades Juveniles en el MTS SEGUNDA PARTE",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#008781", "#cfc08c"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-comunidades-juveniles-en-el-mts-segunda-parte-1.mp3",
@@ -42,7 +42,7 @@ function chillHop() {
         {
             name:"Comunidades Juveniles en el MTS TERCERA PARTE",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#845648","#FDFBDF"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-comunidades-juveniles-en-el-mts-tercera-parte-1.mp3",
@@ -51,7 +51,7 @@ function chillHop() {
         {
             name:"Quien es un Oblato en el Movimiento Tengo Sed",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#578CCE", "#E9D6D0"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-quien-es-un-oblato-en-el-movimiento-tengo-sed.-1-1.mp3",
@@ -60,7 +60,7 @@ function chillHop() {
         {
             name:"Desafíos en la vida comunitaria en el MTS",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#191516", "#82698E"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-desafios-en-la-vida-comunitaria-en-el-mts-podcast-1.mp3",
@@ -69,7 +69,7 @@ function chillHop() {
         {
             name:"EL anhelo de Dios de amar y ser amado",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#7BC8DD", "#D64A59"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-el-anhelo-de-dios-de-amar-y-ser-amado-por-padre-curtis-mc-1.mp3",
@@ -78,7 +78,7 @@ function chillHop() {
         {
             name:"Caminando con María",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#FDE1A9", "#A46874"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-caminando-con-maria-1.mp3",
@@ -87,7 +87,7 @@ function chillHop() {
         {
             name:" El Legado de Madre Teresa al MTS",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-el-legado-de-madre-teresa-al-mts-1.mp3",
@@ -96,7 +96,7 @@ function chillHop() {
         {
             name:"El MTS y su relacion con la Iglesia",
             artist: "Facebook Live",
-            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast.png",
+            cover: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-1.png",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://musiczoneapps.files.wordpress.com/2022/10/podcast-el-mts-y-su-relacion-con-la-iglesia.mp3",
